819e935506d6b5a772c38f0962263a1d7863a216
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2022-08-11 21:06:59 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -12,7 +12,7 @@ USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
 ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
+CHANNELS = ['channelusername',]
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
